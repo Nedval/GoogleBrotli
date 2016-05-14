8c680c953859d69e9fa48f68fe75ba5f9096b3ee
Jrting <Jrting@Nedval.me>
2016-05-15 04:33:15 +0800

    Create Framework Project

---
+++ GoogleBrotli/GoogleBrotli.h
@@ -0,0 +1,18 @@
+//
+//  GoogleBrotli.h
+//  GoogleBrotli
+//
+//  Created by Jrting on 5/15/16.
+//  Copyright © 2016 Modern Wizard Studio. All rights reserved.
+//
+
+@import Foundation
+
+//! Project version number for GoogleBrotli.
+FOUNDATION_EXPORT double GoogleBrotliVersionNumber;
+
+//! Project version string for GoogleBrotli.
+FOUNDATION_EXPORT const unsigned char GoogleBrotliVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleBrotli/PublicHeader.h>
+
